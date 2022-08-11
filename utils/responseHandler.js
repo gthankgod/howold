ba60755a9ebf630db6380e294f909689beb61988
gthankgod <gthankgod>
2022-08-11 15:15:45 +0100

    get age -3

--- utils/responseHandler.js
+++ utils/responseHandler.js
@@ -1,5 +1,18 @@
 function responseHandler(res, data = null, code) {
-  return res.status(code).json({ data });
+  let ValidResponseCode = {
+    200: 1,
+    400: 1,
+    429: 1,
+    500: 1,
+  };
+
+  if (ValidResponseCode[code]) {
+    res.statusCode = code;
+    res.end(JSON.stringify(data));
+  } else {
+    res.statusCode = code;
+    res.end();
+  }
 }
 
 module.exports = responseHandler;
