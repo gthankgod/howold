5b545abb3ee005e75d72f919c7b90ee31dc5a82d
gthankgod <gthankgod>
2022-08-12 01:48:34 +0100

    refactored code to cater for rate limiting headers

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -5,6 +5,7 @@ let ConnectionResetTime = process.env.ConnectionResetTime || 1;
 let numberOfAllowedHits = process.env.numberOfAllowedHits || 3;
 
 const rateLimiter = async (req, res, next) => {
+  let ttl;
   if (!req.$redisConnected) {
     throw new Error("Unable to connect to Redis, please try again");
   }
@@ -13,10 +14,15 @@ const rateLimiter = async (req, res, next) => {
   const ip = getIp(req);
   const numOfRequests = await redisClient.incr(ip);
 
-  if (numOfRequests === 1) redisClient.expire(ip, ConnectionResetTime);
+  if (numOfRequests === 1) {
+    redisClient.expire(ip, ConnectionResetTime);
+    ttl = ConnectionResetTime;
+  } else {
+    ttl = await redisClient.ttl(ip);
+  }
 
   return numOfRequests > numberOfAllowedHits
-    ? responseHandler(res, "Too many request...", 429)
+    ? responseHandler(res, "Too many request...", 429, { ip, ttl })
     : next();
 };
 
--- utils/responseHandler.js
+++ utils/responseHandler.js
@@ -1,18 +1,32 @@
-function responseHandler(res, data = null, code) {
-  let ValidResponseCode = {
-    200: 1,
-    400: 1,
-    429: 1,
-    500: 1,
+function responseHandler(res, data = null, code, options) {
+  let SuccessResponse = () => {
+    return res.status(200).json({ age: data });
   };
 
-  if (ValidResponseCode[code]) {
-    res.statusCode = code;
-    res.end(JSON.stringify(data));
-  } else {
-    res.statusCode = code;
-    res.end();
-  }
+  let RateLimitResponse = () => {
+    res.setHeader("x-ratelimit-limit", 1);
+    res.setHeader("x-ratelimit-remaining", options.ttl);
+    return res.status(429).json({
+      error: `${options.ip} has been rate limited. Please try again in ${options.ttl} seconds`,
+    });
+  };
+  let BadResponse = () => {
+    return res.status(400).json({ error: data });
+  };
+  let ServerResponse = () => {
+    return res.status(500).json({ error: "Server Error. Please try again" });
+  };
+
+  let validCodes = {
+    200: SuccessResponse,
+    400: BadResponse,
+    429: RateLimitResponse,
+    500: ServerResponse,
+  };
+
+  if (!validCodes[code])
+    return res.status(code).json({ error: "Unknown error" });
+  return validCodes[code]();
 }
 
 module.exports = responseHandler;
